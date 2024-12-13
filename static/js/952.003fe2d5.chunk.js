29ff0decc284ffa5a48f7559f256a9fd1867bad3
Orimous-off <160538568+Orimous-off@users.noreply.github.com>
2024-12-13 19:52:07 +0300

    Add files via upload

---
+++ static/js/952.003fe2d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreviews_films=self.webpackChunkreviews_films||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.003fe2d5.chunk.js.map
